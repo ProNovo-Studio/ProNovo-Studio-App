d92d29d1843ab329eb06ca26b72586fb1cbb9338
apinto17 <apinto0704@gmail.com>
2025-12-28 17:55:46 -0800

    updated gitignore

--- frontend/ui_v2/dist/assets/index-BNz5Si_e.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-sbjvgp5E.js";function t(){return o.jsx("div",{children:"Splash Page!"})}export{t as component};
--- frontend/ui_v2/dist/assets/index-BonWLMoG.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-sbjvgp5E.js";function n(){return o.jsx("div",{children:'Hello "/pipelines/"!'})}export{n as component};
