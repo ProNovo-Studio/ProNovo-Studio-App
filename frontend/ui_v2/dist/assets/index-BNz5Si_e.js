6a63e05505d2bd722b9998dca3d289c93671c6e4
apinto17 <apinto0704@gmail.com>
2025-12-28 17:52:57 -0800

    fixed build

---
+++ frontend/ui_v2/dist/assets/index-BNz5Si_e.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-sbjvgp5E.js";function t(){return o.jsx("div",{children:"Splash Page!"})}export{t as component};
---
+++ frontend/ui_v2/dist/assets/index-BonWLMoG.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-sbjvgp5E.js";function n(){return o.jsx("div",{children:'Hello "/pipelines/"!'})}export{n as component};
